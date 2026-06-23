2bc562f6d8d5b27e5981e7f93f46a0c176de5843
Hugo Marotta <humtta@proton.me>
2026-06-22 21:29:55 -0300

    Scaffold tests for dedent.Dedentf

--- dedent_test.go
+++ dedent_test.go
@@ -20,3 +20,21 @@ func TestDedent(t *testing.T) {
 		})
 	}
 }
+
+func TestDedentf(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+		s    string
+		a    []any
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Dedentf(tt.s, tt.a...)
+			if got != tt.want {
+				t.Errorf("Dedent(%q, %v)\ngot: %q\nwant: %q", tt.s, tt.a, got, tt.want)
+			}
+		})
+	}
+}
