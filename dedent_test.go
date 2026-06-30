63fa5d06c266fad352d0b8e973e570c18948ed44
Hugo Marotta <humtta@proton.me>
2026-06-30 17:52:24 -0300

    Remove name field from dedent.D tests

--- dedent_test.go
+++ dedent_test.go
@@ -6,18 +6,15 @@ import (
 
 func TestD(t *testing.T) {
 	tests := []struct {
-		name string
 		s    string
 		want string
 	}{}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := D(tt.s)
-			if got != tt.want {
-				t.Errorf("D(%q)\ngot: %q\nwant: %q", tt.s, got, tt.want)
-			}
-		})
+		got := D(tt.s)
+		if got != tt.want {
+			t.Errorf("D(%q)\ngot: %q\nwant: %q", tt.s, got, tt.want)
+		}
 	}
 }
 
