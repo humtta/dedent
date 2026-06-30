91f43025c918e27c22a88c3bb605ff0615e8feed
Hugo Marotta <humtta@proton.me>
2026-06-30 17:52:59 -0300

    Remove name field from dedent.Df tests

--- dedent_test.go
+++ dedent_test.go
@@ -20,18 +20,15 @@ func TestD(t *testing.T) {
 
 func TestDf(t *testing.T) {
 	tests := []struct {
-		name string
 		s    string
 		a    []any
 		want string
 	}{}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := Df(tt.s, tt.a...)
-			if got != tt.want {
-				t.Errorf("Df(%q, %v)\ngot: %q\nwant: %q", tt.s, tt.a, got, tt.want)
-			}
-		})
+		got := Df(tt.s, tt.a...)
+		if got != tt.want {
+			t.Errorf("Df(%q, %v)\ngot: %q\nwant: %q", tt.s, tt.a, got, tt.want)
+		}
 	}
 }
