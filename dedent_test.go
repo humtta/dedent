77162cbf2516f5372fe2ccd0cbe88ecc056955bc
Hugo Marotta <humtta@proton.me>
2026-06-30 17:40:49 -0300

    Fix dedent.TestDedentf failure message

--- dedent_test.go
+++ dedent_test.go
@@ -33,7 +33,7 @@ func TestDedentf(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Dedentf(tt.s, tt.a...)
 			if got != tt.want {
-				t.Errorf("Dedent(%q, %v)\ngot: %q\nwant: %q", tt.s, tt.a, got, tt.want)
+				t.Errorf("Dedentf(%q, %v)\ngot: %q\nwant: %q", tt.s, tt.a, got, tt.want)
 			}
 		})
 	}
